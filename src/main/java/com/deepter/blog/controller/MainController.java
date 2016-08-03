7e3c58eb9beef1bfb6d837b83e528c9a3ea9702c
Administrator <Administrator@admin-PC>
2016-08-03 21:59:54 +0800

    subm

--- src/main/java/com/deepter/blog/controller/MainController.java
+++ src/main/java/com/deepter/blog/controller/MainController.java
@@ -1,5 +1,21 @@
 package com.deepter.blog.controller;
 
-public class MainController {
+import java.util.Map;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping(value="home")
+	public String toHome(HttpSession session,Map<String, Object> map,Model model){
+		System.out.println(session.getId());
+		model.addAttribute("name", "Session的名字是：");
+		map.put("sessionId", session.getId());
+		return "home";
+	}
 }
